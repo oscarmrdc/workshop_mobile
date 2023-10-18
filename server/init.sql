8f7dca48ea19c6b61a95135b54ba7e85da27062d
oscarmrdc <37247079+oscarmrdc@users.noreply.github.com>
2023-10-18 16:42:54 -0500

    Add files via upload

---
+++ server/init.sql
@@ -0,0 +1,4 @@
+USE darkdemo;
+INSERT INTO tutorials (id, dni, hashdni, saldo, activo, createdAt, updatedAt) VALUES (1, '12345679', 'b759803bc6037a05e6564b6447a755b7f3862ba4d0d746785dbe133dcb6c8f4d', '200.00', 1, curdate(), curdate());
+INSERT INTO tutorials (id, dni, hashdni, saldo, activo, createdAt, updatedAt) VALUES (3, '12345678', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', '400.00', 1, curdate(), curdate());
+INSERT INTO tutorials (id, dni, hashdni, saldo, activo, createdAt, updatedAt) VALUES (2, '12345677', '79c24954807c83c719a99fa71958ad1afb7b17a2fe03d4773d383797dfcbfbf3', '2000.00', 1, curdate(), curdate());
